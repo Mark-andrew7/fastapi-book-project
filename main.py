55e496e6d6825345cabbd491e45eed56de35a36a
Mark-andrew7 <kamaumark19@gmail.com>
2025-02-13 19:49:15 +0000

    Added fake endpoint to test CI/CD deployment

--- main.py
+++ main.py
@@ -21,3 +21,8 @@ app.include_router(api_router, prefix=settings.API_PREFIX)
 async def health_check():
     """Checks if server is active."""
     return {"status": "active"}
+
+@app.get("/fake-endpoint")
+async def fake_endpoint():
+    return {"message": "This is a test deployment!"}
+
